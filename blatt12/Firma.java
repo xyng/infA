36062e1238f21c1aaed3d695dda3315d424cc308
Daniel Melchior <dmelchior@uni-osnabrueck.de>
2016-01-18 14:14:27 +0100

    add comments

--- blatt12/Firma.java
+++ blatt12/Firma.java
@@ -13,46 +13,91 @@ public class Firma implements Comparable {
 	private String name;
 	private long annual, usid;
 
+	/**
+	 * Erstellt neue Firma
+	 *
+	 * @param name Name der Firma
+	 * @param annual Jahresumsatz
+	 * @param usid Umsatzsteueridentifikationsnummer
+	 */
 	Firma(String name, long annual, long usid) {
 		setName(name);
 		setAnnual(annual);
 		setUSID(usid);
 	}
 
+	/**
+	 * Namen der Firma ändern
+	 * @param name Name der Firma
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Jahresumsatz der Firma ändern
+	 * @param annual Jahresumsatz
+	 */
 	public void setAnnual(long annual) {
 		this.annual = annual;
 	}
 
+	/**
+	 * Umsatzsteueridentifikationsnummer der Firma ändern
+	 * @param usid Umsatzsteueridentifikationsnummer
+	 */
 	public void setUSID(long usid) {
 		this.usid = usid;
 	}
 
+	/**
+	 * Gibt Namen der Firma zurück
+	 * @return name
+	 */
 	public String getName() {
 		return this.name;
 	}
 
+	/**
+	 * Gibt Jahresumsatz der Firma zurück
+	 * @return Jahresumsatz
+	 */
 	public long getAnnual() {
 		return this.annual;
 	}
 
+	/**
+	 * Gibt Umsatzsteueridentifikationsnummer der Firma zurück
+	 * @return Umsatzsteueridentifikationsnummer
+	 */
 	public long getUSID() {
 		return this.usid;
 	}
 
+	/**
+	 * Gibt Daten als String zurück
+	 * @return Daten der Firma
+	 */
 	public String toString() {
 		return	"Frima: " + this.name +
 				" Jahresumsatz: " + this.annual +
 				" Umsatzsteueridentifikationsnummer: " + this.usid;
 	}
 
+	/**
+	 * Erzeugt HashCode des Datensatzes
+	 * @return HashCode
+	 */
 	public int hashCode() {
+		// Long zu int machen
 		return (int) this.usid;
 	}
 
+	/**
+	 * Prüft gleichheit von zwei Firmen
+	 * @param  x andere Firma
+	 * @return   true falls identisch, false wenn nicht.
+	 */
 	public boolean equals(Object x) {
 		if(x == null) {
 			return false;
@@ -61,6 +106,13 @@ public class Firma implements Comparable {
 		return this.usid == ((Firma) x).getUSID();
 	}
 
+	/**
+	 * Vergleicht zwei Firmen
+	 * @param  x andere Firma
+	 * @return   < 0 falls x größer, > 0 falls x kleiner, 0 falls gleich.
+	 *
+	 * @throws  RuntimeException wenn null gegeben wird.
+	 */
 	public int compareTo(Object x) {
 		if(x == null) {
 			throw new RuntimeException("Null given.");
--- blatt12/OffeneHashtabelle.java
+++ blatt12/OffeneHashtabelle.java
@@ -12,6 +12,9 @@ import AlgoTools.IO;
 public class OffeneHashtabelle implements Menge {
 	private Liste[] lists;
 
+	/**
+	 * Initialisiert Hashtabelle mit 7 Listen
+	 */
 	OffeneHashtabelle() {
 		lists = new Liste[7];
 
@@ -20,8 +23,14 @@ public class OffeneHashtabelle implements Menge {
 		}
 	}
 
+	/**
+	 * Prüft ob Tabelle leer
+	 * @return true wenn leer, false sonst.
+	 */
 	public boolean empty() {
+		// Alle Listen abklappern
 		for (int i = 0; i < 7; i++) {
+			// Und überprüfen, ob Liste leer
 			if (!lists[i].empty()) {
 				return false;
 			}
@@ -30,36 +39,67 @@ public class OffeneHashtabelle implements Menge {
 		return true;
 	}
 
+	/**
+	 * Prüfen ob Element in Tabelle liegt
+	 * @param  x zu findendes Element
+	 * @return   Comparable, falls Element gefunden, null sonst.
+	 */
 	public Comparable lookup(Comparable x) {
+		// Element muss in Liste mit id hashCode % 7 liegen
 		Liste list = lists[((Firma) x).hashCode() % 7];
+
+		// Ist diese nicht leer
 		if(!list.empty()) {
+			// Zunächst zurücksetzen
 			list.reset();
+			// Dann so lange laufen bis Endpos gefunden
 			while(!list.endpos()) {
 				Comparable cur = (Comparable) list.elem();
+				// Wenn Elemente gleich
 				if (cur.compareTo(x) == 0) {
+					// Gefundenes Element zurückgeben
 					return cur;
 				}
+				// Ansonsten weiterlaufen
 				list.advance();
 			}
 		}
-
+		// Element nicht gefunden -> null zurückgeben
 		return null;
 	}
 
+	/**
+	 * Neue Firma in Tabelle einfügen
+	 * @param  x Firma
+	 * @return   true fals element eingefügt, false wenn nicht.
+	 */
 	public boolean insert(Comparable x) {
+		// Existiert Element schon?
 		if(lookup(x) != null) {
+			// Wenn ja -> nichts neues einfügen und false zurückgeben
 			return false;
 		}
-
+		// Ansonsten Objekt in entsprechende Liste einsetzen
 		lists[((Firma) x).hashCode() % 7].insert(x);
+		// Und true zurückgeben
 		return true;
 	}
 
+	/**
+	 * Objekt aus Tabelle löschen
+	 * @param  x zu löschendes Objekt
+	 * @return   True wenn element gefunden und gelöscht, false sonst.
+	 */
 	public boolean delete(Comparable x) {
+		// Existiert zu löschendes Element=
 		if(lookup(x) != null) {
+			// Wenn ja -> löschen
 			lists[((Firma) x).hashCode() % 7].delete();
+			// Und true zurückgeben
 			return true;
 		}
+
+		// Sonst false zurückgeben
 		return false;
 	}
 }
